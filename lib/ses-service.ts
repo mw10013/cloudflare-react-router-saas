bdca1833bb4d1a63c76e8523d32dce2dd9aaf7eb
Michael Wu <mw10013@gmail.com>
2025-11-11 11:54:20 -0500

    refactor: remove EMAIL_WHITE_LIST from .env.example and update ses-service to use process.env for email whitelist

--- lib/ses-service.ts
+++ lib/ses-service.ts
@@ -63,8 +63,8 @@ export function createSesService(): SesService {
   invariant(env.AWS_SES_SECRET_ACCESS_KEY, "Missing AWS_SES_SECRET_ACCESS_KEY");
   invariant(env.AWS_SES_ENDPOINT, "Missing AWS_SES_ENDPOINT");
   invariant(env.AWS_SES_REGION, "Missing AWS_SES_REGION");
-  const emailWhitelist = env.EMAIL_WHITE_LIST
-    ? env.EMAIL_WHITE_LIST.split(",")
+  const emailWhitelist = process.env.EMAIL_WHITE_LIST
+    ? process.env.EMAIL_WHITE_LIST.split(",")
     : [];
 
   const aws = new AwsClient({
